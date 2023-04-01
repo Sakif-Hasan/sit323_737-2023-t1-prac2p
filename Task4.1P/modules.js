7e07940d587eee02cafd6cc38d5c75d1af1ebb42
Sakif_hasan <51274425+Sakif-Hasan@users.noreply.github.com>
2023-04-01 18:40:05 +1100

    Task 4.1P

---
+++ Task4.1P/modules.js
@@ -0,0 +1,23 @@
+// this file holds the math functions
+
+add = function addition(var1,var2){
+    result = var1 + var2
+    return result
+}
+
+sub = function subtraction(var1,var2){
+    result = var1 - var2
+    return result
+}
+
+mul = function multiplication(var1,var2){
+    result = var1 * var2
+    return result
+}
+
+div = function division(var1,var2){
+    result = var1 / var2
+    return result
+}
+
+module.exports.add = add
---
+++ Test Files/demo.js
@@ -0,0 +1,8 @@
+var myNum = 32;
+function myFunction() {
+    var myNum = 5;
+    return myNum;
+    var myNum = 10;
+    return myNum; }
+
+myFunction();
